d5d003cf1aeb9a8228291ff07e2a854166a4812d
LuisSantamariaUPB <luisestebansantamaria@outlook.com>
2020-07-31 19:46:51 -0500

    Week 1 First exercise Initial Commit

--- Java Programming 4th Course - Principles of Software Design/Semana 1/Magnitude Filter Code/Main.java
+++ Java Programming 4th Course - Principles of Software Design/Semana 1/Magnitude Filter Code/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        Testing test = new Testing();
+        test.testEarthQuakeClientBigQuakes();;
+    }
 }
--- Java Programming 4th Course - Principles of Software Design/Semana 1/Magnitude Filter Code/Testing.java
+++ Java Programming 4th Course - Principles of Software Design/Semana 1/Magnitude Filter Code/Testing.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Testing {
+
+    public void testEarthQuakeClientCreateCSV() {
+        EarthQuakeClient earthQuakeClient = new EarthQuakeClient();
+        earthQuakeClient.createCSV();
+    }
+
+    public void testEarthQuakeClientBigQuakes() {
+        EarthQuakeClient earthQuakeClient = new EarthQuakeClient();
+        earthQuakeClient.bigQuakes();
+    }
 }
