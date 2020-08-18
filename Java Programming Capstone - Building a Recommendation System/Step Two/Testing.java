d2a56b43e9464d8fcac895e50c6025b81f4949d4
LuisSantamariaUPB <luisestebansantamaria@outlook.com>
2020-08-18 18:35:07 -0500

    Step Two Completed

---
+++ Java Programming Capstone - Building a Recommendation System/Step Two/Testing.java
@@ -0,0 +1,31 @@
+
+/*
+    Class for testing methods of all classes
+ */
+
+public class Testing {
+
+    public static void main(String[] args) {
+//        testFirstRatings();
+        testMovieRunnerAverage();
+//        testSecondRatings();
+    }
+
+    public static void testFirstRatings() {
+        FirstRatings firstRatings = new FirstRatings();
+//        firstRatings.testLoadMovies();
+        firstRatings.testLoadRaters();
+    }
+
+    public static void testSecondRatings() {
+        SecondRatings sr = new SecondRatings();
+        sr.getAverageRatings(2);
+    }
+
+    public static void testMovieRunnerAverage() {
+        MovieRunnerAverage mr = new MovieRunnerAverage();
+//        mr.printAverageRatings();
+        mr.getAverageRatingOneMovie();
+    }
+
+}
